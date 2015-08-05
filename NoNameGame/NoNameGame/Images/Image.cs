db7517cabea8b38b1e7d84e3a13ab10d9a80c0bd
Marc Satkowski <marc.satkowski@yahoo.de>
2015-08-06 01:44:57 +0200

    XmlManager und Image hinzugefügt

---
+++ NoNameGame/NoNameGame/Images/Image.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Xml.Serialization;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NoNameGame.Images
+{
+    public class Image
+    {
+        Vector2 origin;
+        Vector2 scaleOrigin;
+        ContentManager content;
+        Vector2 position;
+
+        [XmlIgnore]
+        public Texture2D Texture;
+        public string Path;
+        public float Scale;
+        public float Rotation;
+        public float Alpha;
+        public Color Color;
+        public Vector2 Position
+        {
+            get { return position; }
+            set
+            {
+                position = value;
+                OnPositionChange(position, null);
+            }
+        }
+        public Rectangle SourceRectangle;
+        public Rectangle CurrentRectangle;
+        public Rectangle PrevRectangle;
+        public int MergeOffset;
+
+        public event EventHandler OnPositionChange;
+
+        public Image ()
+        {
+            Position = Vector2.Zero;
+            Scale = 1.0f;
+            Alpha = 1.0f;
+            Rotation = 0.0f;
+            Path = String.Empty;
+            SourceRectangle = Rectangle.Empty;
+            PrevRectangle = Rectangle.Empty;
+            CurrentRectangle = Rectangle.Empty;
+            MergeOffset = 0;
+            Color = Color.White;
+            origin = Vector2.Zero;
+            scaleOrigin = Vector2.Zero;
+        }
+
+        public void LoadContent (ContentManager content)
+        {
+            this.content = content;
+
+            if (Path != String.Empty)
+                Texture = content.Load<Texture2D>(Path);
+        }
+
+        public void UnloadContent ()
+        {
+            content.Unload();
+        }
+
+        public void Update (GameTime gameTime)
+        {
+            origin = new Vector2(SourceRectangle.Width / 2, SourceRectangle.Height / 2);
+            scaleOrigin = new Vector2(SourceRectangle.Width * Scale / 2, SourceRectangle.Height * Scale / 2);
+
+            PrevRectangle = CurrentRectangle;
+            CurrentRectangle = new Rectangle((int)(Position.X), (int)(Position.Y),
+                                             (int)(SourceRectangle.Width * Scale), (int)(SourceRectangle.Height * Scale));
+        }
+
+        public void Draw (SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position + origin, SourceRectangle, Color.White, Rotation, origin, Scale, SpriteEffects.None, 0.0f);
+        }
+    }
+}
---
+++ NoNameGame/NoNameGame/Managers/XmlManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using System.Xml.Serialization;
+
+namespace NoNameGame.Managers
+{
+    public class XmlManager<T>
+    {
+        public Type Type;
+
+        public XmlManager ()
+        {
+            Type = typeof(T);
+        }
+
+        /// <summary>
+        /// Lädt ein Object vom Typ "T" aus dem Pfad.
+        /// </summary>
+        /// <param name="path">Pfad zur XML zum Laden</param>
+        /// <returns>Eine Instanz der Klasse T</returns>
+        public T Load (string path)
+        {
+            T instance;
+            using (TextReader reader = new StreamReader(path))
+            {
+                XmlSerializer xml = new XmlSerializer(Type);
+                instance = (T)xml.Deserialize(reader);
+            }
+            return instance;
+        }
+
+        public void Save (string path, object obj)
+        {
+            using (TextWriter writer = new StreamWriter(path))
+            {
+                XmlSerializer xml = new XmlSerializer(Type);
+                xml.Serialize(writer, obj);
+            }
+        }
+    }
+}
