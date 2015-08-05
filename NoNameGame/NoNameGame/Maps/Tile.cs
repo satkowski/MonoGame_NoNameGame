c0dbf64b65c9b2e762cb72b4f5a76408c93a655e
Marc Satkowski <marc.satkowski@yahoo.de>
2015-08-06 01:45:14 +0200

    Tiles für die Map hinzugefügt

---
+++ NoNameGame/NoNameGame/Maps/Tile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+namespace NoNameGame.Maps
+{
+    public class Tile
+    {
+        public enum TileRotation 
+        {
+            None = 0,
+            Clockwise90 = 1,
+            Clockwise180 = 2,
+            Clockwise270 = 3
+        }
+
+        public TileRotation Rotation;
+        public float Scale;
+        public Vector2 DestinationPosition;
+        public Vector2 TileSheetPosition;
+
+        public Tile ()
+        {
+            Rotation = TileRotation.None;
+            Scale = 1.0f;
+            DestinationPosition = Vector2.Zero;
+            TileSheetPosition = Vector2.Zero;
+        }
+    }
+}
