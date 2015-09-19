d42787452adf8ec4193ca09d3676a44582626b9f
Marc Satkowski <marc.satkowski@yahoo.de>
2015-09-19 17:01:17 +0200

    Map und ZoominEffect hinzugefügt

---
+++ NoNameGame/NoNameGame/Maps/Effects/MapEffect.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace NoNameGame.Maps.Effects
+{
+    public abstract class MapEffect
+    {
+        protected Map map;
+
+        public bool IsActive;
+
+        public MapEffect()
+        {
+            IsActive = false;
+        }
+
+        public virtual void LoadContent(ref Map map)
+        {
+            this.map = map;
+        }
+
+        public virtual void UnloadContent()
+        {
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+        }
+    }
+}
