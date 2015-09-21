39575641a346c42bd53d9cfa1052fa44b81aa9a7
Marc Satkowski <marc.satkowski@yahoo.de>
2015-09-21 12:45:13 +0200

    AutomatedEntitys hinzugefügt und Abilities hinzugefügt.

---
+++ NoNameGame/NoNameGame/Entities/Abilities/EntityAbility.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace NoNameGame.Entities.Abilities
+{
+    public class EntityAbility
+    {
+        Entity entity;
+
+        public bool IsActive;
+
+        public EntityAbility()
+        {
+            IsActive = false;
+        }
+
+        public virtual void LoadContent(ref AutomatedEntity entity)
+        {
+            this.entity = entity;
+        }
+
+        public virtual void UnloadContent()
+        {
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+        }
+    }
+}
