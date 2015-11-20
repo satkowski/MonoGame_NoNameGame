0e4321800d112b8f96723412c16bd1b53a08dc1d
Marc Satkowski <marc.satkowski@mail.de>
2015-11-21 00:45:16 +0100

    OOBB erweitert.

--- NoNameGame/Components/Shapes/OBBShape.cs
+++ NoNameGame/Components/Shapes/OBBShape.cs
@@ -1,4 +1,5 @@
-﻿using NoNameGame.Maps;
+﻿using Microsoft.Xna.Framework;
+using NoNameGame.Maps;
 using System;
 using System.Xml.Serialization;
 
@@ -7,7 +8,28 @@ namespace NoNameGame.Components.Shapes
     [XmlInclude(typeof(OBBShape))]
     public class OBBShape : AABBShape
     {
-        public float Rotation;
+        private float rotation;
+        public float Rotation
+        {
+            get { return rotation; }
+            set
+            {
+                rotation = value;
+                rotationX = new Vector2((float)Math.Cos(rotation), -(float)Math.Sin(rotation));
+                rotationX = new Vector2((float)Math.Sin(rotation), (float)Math.Cos(rotation));
+            }
+        }
+
+        protected Vector2 rotationX;
+        protected Vector2 rotationY;
+        public new Vector2 Left
+        { get { return Vector2.Transform(new Vector2(base.Left, 0), Matrix.CreateRotationZ(MathHelper.ToRadians(rotation))); } }
+        public new Vector2 Top
+        { get { return Vector2.Transform(new Vector2(0, base.Top), Matrix.CreateRotationZ(MathHelper.ToRadians(rotation))); } }
+        public new Vector2 Right
+        { get { return Vector2.Transform(new Vector2(base.Right, 0), Matrix.CreateRotationZ(MathHelper.ToRadians(rotation))); } }
+        public new Vector2 Bottom
+        { get { return Vector2.Transform(new Vector2(0, base.Bottom), Matrix.CreateRotationZ(MathHelper.ToRadians(rotation))); } }
 
         public OBBShape()
         {
